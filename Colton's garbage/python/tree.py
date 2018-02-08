6dfff3ff50e607a2e32e1cc5f81e62229951b511
Colton <31966580+aLuxio@users.noreply.github.com>
2018-02-08 17:44:17 -0600

    started making a binary search tree

---
+++ Colton's garbage/python/tree.py
@@ -0,0 +1,12 @@
+class Node(object):
+    def __init__(self):
+        self.value = 0
+        self.left = None
+        self.right = None
+
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
+
+    
